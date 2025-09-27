b91eebfaf95b8f89fa9a25f6a7ecf9e7365c01db
Abdifatah Ismail <abdifatahismail886@gmail.com>
2025-09-27 20:25:05 +0300

    Second

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-     <h1>Hello world</h1>
-    </>
+    <div className="container">
+     <h1>Task Management System</h1>
+     <div className="main-content">
+      <div className="tasks-column">
+        <h2>Current Tasks</h2>
+        <table>
+          <thead>
+            <tr>
+            <th></th>
+            <th>Task</th>
+            <th>Created</th>
+            <th>Due Date</th>
+            <th>Prioraty</th>
+            <th>Actions</th>
+
+          </tr>
+          </thead>
+          
+        </table>
+      </div>
+
+    
+
+     </div>
+
+    </div>
   )
 }
 
