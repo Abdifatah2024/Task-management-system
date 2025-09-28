d086e314687284a0ab58c036363fb97775d6cba4
Abdifatah Ismail <abdifatahismail886@gmail.com>
2025-09-28 05:52:12 +0300

    All parts design

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { FaEdit } from "react-icons/fa";   // FontAwesome
+import { FaTrash } from "react-icons/fa";   // FontAwesome
 function App() {
 
   return (
@@ -18,11 +20,78 @@ function App() {
 
           </tr>
           </thead>
+          <tbody>
+            <tr>
+              <td><input type="checkbox" /></td>
+              <td>Login form</td>
+              <td>27.09,2025</td>
+              <td>30.09.2025</td>
+              <td className="priority-medium ">Medium</td>
+             <td className="flex gap-2">
+  <button className="btn-delete">
+    <FaTrash size={18} />
+  </button>
+  <button className="btn-edit">
+    <FaEdit size={18} />
+  </button>
+</td>
+
+            </tr>
+          </tbody>
           
         </table>
-      </div>
+        <div className="completed-tasks">
+          <h2 className="completed">Completed Task</h2>
+              <table>
+          <thead>
+            <tr>
+            <th></th>
+            <th>Task</th>
+            <th>Created</th>
+            <th>Completed Date</th>
+            <th>Prioraty</th>
+            <th>Actions</th>
 
-    
+          </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td><input type="checkbox" /></td>
+              <td>Login form</td>
+              <td>27.09,2025</td>
+              <td>30.09.2025</td>
+              <td className="priority-medium ">Medium</td>
+             <td className="flex gap-2">
+  <button className="btn-delete">
+    <FaTrash size={18} />
+  </button>
+  <button className="btn-edit">
+    <FaEdit size={18} />
+  </button>
+</td>
+
+            </tr>
+          </tbody>
+          
+        </table>
+        </div>
+      </div>
+<div className="task-form">
+  <h2>Add Task</h2>
+  <input type="text" placeholder="Write here your task"/>
+  <label htmlFor="">Dua date</label>
+  <input type="date" />
+  <label htmlFor="">Prioraty</label>
+  <select name="" id="">
+    <option value="">Select</option>
+    <option value="">Medium</option>
+    <option value="">Low</option>
+    <option value="">High</option>
+  </select>
+
+  <button className="Save-btn">Save</button>
+</div>
+   
 
      </div>
 
